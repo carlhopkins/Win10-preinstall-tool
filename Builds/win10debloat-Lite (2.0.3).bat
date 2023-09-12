ff6994d91b4a95d7ce327b413806cc62cc1a21f9
Carl Hopkins <mail@carlhopkins.com>
2023-09-12 11:56:46 +0100

    2.0.3 Loader update
    tweaks and added version for Warp edition

---
+++ Builds/win10debloat-Lite (2.0.3).bat
@@ -0,0 +1,4 @@
+REM Windows 10 Debloat Loader - Lite version - Copyright (c) 2023 Carl Hopkins
+@echo off
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""iwr -useb https://raw.githubusercontent.com/carlhopkins/Win10-preinstall-tool/main/win10debloat-Lite.ps1 | iex""' -Verb RunAs}"
+TIMEOUT /T 2
