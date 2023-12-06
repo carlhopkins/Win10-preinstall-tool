779e43d987260d33f6e60506208fdfd46cfbb2ac
Carl Hopkins <mail@carlhopkins.com>
2023-12-06 17:28:05 +0000

    v4 updates

---
+++ Win10PreP.bat
@@ -0,0 +1,9 @@
+@echo off 
+REM Windows 10 Machine Pre-Installation PreP Tool - Launcher Script - Copyright (c) 2023 Carl Hopkins
+
+ECHO +====================================================+
+ECHO +            Windows 10 PreP Tool Loader             +
+ECHO +====================================================+
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""iwr -useb https://raw.githubusercontent.com/carlhopkins/Win10-preinstall-tool/main/current/Win10PreP.ps1 | iex""' -Verb RunAs}"
+TIMEOUT /T 2
